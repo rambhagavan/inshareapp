517cfcaa456bcc05300f908be19a34a57ca54952
rambhagavan <shivaramyadav12@gmail.com>
2023-08-22 17:27:40 +0530

    add cron job schedular

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const connectDB=require('./config/db');
 connectDB;
 async function fetchData(){
    
-    const pastDate=new Date(Date.now()-(60*1000));
+    const pastDate=new Date(Date.now()-(24*60*60*1000));
     const files=await File.find({createdAt:{$lt:pastDate}});
     if(files.length){
         
